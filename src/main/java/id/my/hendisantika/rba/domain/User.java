c80009c564e932050650d94009639fa112d58ec4
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:09:01 +0700

    feat: Add User Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rba/domain/User.java
@@ -0,0 +1,51 @@
+package id.my.hendisantika.rba.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.HashSet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-role-based-authorization
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 10.08
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+
+    private boolean enabled;
+}
