d831ba6394d89dc35f5901cb4e3d33f95d513bbe
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:15:34 +0700

    feat: Add getAuthenticatedUser in TestController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rba/controller/TestController.java
+++ src/main/java/id/my/hendisantika/rba/controller/TestController.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika.rba.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 10.14
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @EnableMethodSecurity
 public class TestController {
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/test")
+    public Authentication getAuthenticatedUser() {
+        log.info("test {}", SecurityContextHolder.getContext().getAuthentication());
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
 }
