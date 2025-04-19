6f67a37b73b10498b8ebec0fea354e1c2d61188a
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:18:41 +0700

    feat: Add getAuthenticatedUser_AdminRole_Success in TestControllerTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/rba/controller/TestControllerTest.java
+++ src/test/java/id/my/hendisantika/rba/controller/TestControllerTest.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.rba.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,15 @@ import org.springframework.test.web.servlet.MockMvc;
 class TestControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    void getAuthenticatedUser_AdminRole_Success() throws Exception {
+        Authentication authentication = new TestingAuthenticationToken("user", "password", "ADMIN");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String auth = "{\"authorities\":[{\"authority\":\"ADMIN\"}],\"details\":null,\"authenticated\":true,\"credentials\":\"password\",\"principal\":\"user\",\"name\":\"user\"}";
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(auth));
+    }
 }
