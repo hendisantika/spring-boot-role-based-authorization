d6457a1f67073bb5efa4739de7d6b00f5cd3d833
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:14:08 +0700

    feat: Add UserDetailsServiceImpl🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rba/security/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package id.my.hendisantika.rba.security;
+
+import id.my.hendisantika.rba.domain.User;
+import id.my.hendisantika.rba.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-role-based-authorization
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 10.13
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserDetailsImpl(user);
+    }
+}
