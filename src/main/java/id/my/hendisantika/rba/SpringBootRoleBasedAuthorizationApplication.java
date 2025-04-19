5ca9511180f9379df22afc22ac0a8d28133d00eb
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:06:25 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rba/SpringBootRoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.rba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRoleBasedAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRoleBasedAuthorizationApplication.class, args);
+    }
+
+}
