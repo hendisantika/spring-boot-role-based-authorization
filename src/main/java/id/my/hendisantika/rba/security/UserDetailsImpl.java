6bdbc75f718cb91429bb136481084eca4138522d
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:13:42 +0700

    feat: Add UserDetailsImpl🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rba/security/UserDetailsImpl.java
@@ -0,0 +1,71 @@
+package id.my.hendisantika.rba.security;
+
+import id.my.hendisantika.rba.domain.Role;
+import id.my.hendisantika.rba.domain.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-role-based-authorization
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 10.13
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
