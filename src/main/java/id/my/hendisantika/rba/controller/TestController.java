e36b7eb65a5a88a3fa7a083e5405ddcf46abb31a
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 10:14:58 +0700

    feat: Add TestController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rba/controller/TestController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.rba.controller;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-role-based-authorization
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 10.14
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@EnableMethodSecurity
+public class TestController {
+}
